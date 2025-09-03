6d6c080091126a7bde2cbc5ae521888e0b232670
kumarvivek93296-alt <kumarvivek93296@gmail.com>
2025-09-03 19:29:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCYneb7heYJU8D6WranVq26jRCm33KTEFA",
+  authDomain: "tricolor-trends.firebaseapp.com",
+  projectId: "tricolor-trends",
+  storageBucket: "tricolor-trends.firebasestorage.app",
+  messagingSenderId: "320568389289",
+  appId: "1:320568389289:web:22c9cce20c2a499a6a43b8",
+  measurementId: "G-P5YKMPYK5N"
 };
-export {firebaseConfig};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
